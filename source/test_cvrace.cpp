23ea74c44c4bc5517cc30f9e4932b35045d205e4
Nico Josuttis <nico@josuttis.de>
2019-01-13 17:16:56 +0100

    document test_cvrace.cpp better

--- source/test_cvrace.cpp
+++ source/test_cvrace.cpp
@@ -68,18 +68,43 @@ void testCVDeadlock()
 
     t2.join();
     
-
-    
-    
   } // leave scope of t1 without join() or detach() (signals cancellation)
   std::cout << "\n*** OK" << std::endl;
 }
 
 
-//------------------------------------------------------
 
-#ifndef ORIG_TEST
-std::condition_variable_any2* cv;
+//------------------------------------------------------
+// Test-Case by Howard Hinnant
+// - emails 8.-9.11.18
+// Original problem:
+//  There's a bug in condition_variable_any2 that I don't think impacts the implementation of jthread.
+//  However this is such a complex subject that no stone should be left unturned.
+// 
+//     ~condition_variable_any();
+// 
+//     Requires: There shall be no thread blocked on *this.
+//     [Note: That is, all threads shall have been notified; they may subsequently block on the lock specified in the wait.
+//            This relaxes the usual rules, which would have required all wait calls to happen before destruction.
+//            Only the notification to unblock the wait needs to happen before destruction.
+//            The user should take care to ensure that no threads wait on *this once the destructor has been started,
+//            especially when the waiting threads are calling the wait functions in a loop or using the overloads of
+//            wait, wait_for, or wait_until that take a predicate.
+//      end note]
+//
+// That big long note means ~condition_variable_any() can execute before a signaled thread returns from a wait.
+// If this happens with condition_variable_any2, that waiting thread will attempt to lock the destructed mutex mut.
+// To fix this, there must be shared ownership of the data member mut between the condition_variable_any object
+// and the member functions wait (wait_for, etc.).
+// 
+// libc++'s implementation gets this right: https://github.com/llvm-mirror/libcxx/blob/master/include/condition_variable
+// 
+// It holds the data member mutex with a shared_ptr<mutex> instead of mutex directly, and the wait functions
+// create a local shared_ptr<mutex> copy on entry so that if *this destructs out from under the thread executing
+// the wait function, the mutex stays alive until the wait function returns.
+//------------------------------------------------------
+#ifndef ORIG_CVANY_RACE_TEST
+std::condition_variable_any2* cv = nullptr;
 std::mutex m;
 bool f_ready = false;
 bool g_ready = false;
@@ -104,7 +129,14 @@ void g() {
 
 void testCVAnyMutex() 
 {
-    cv = new std::condition_variable_any2;
+  std::cout << "*** start testCVAnyMUtex() ORIG" << std::endl;
+    // AW 9.11.18: 
+    // Writing over the deleted memory is undefined behaviour. In particular,
+    // it can destroy the heap data structure, and cause other problems.
+    // If you replace new/delete with malloc and free, then it's OK
+    //cv = new std::condition_variable_any2;
+    void* raw=malloc(sizeof(std::condition_variable_any2));
+    cv = new(raw) std::condition_variable_any2;
     std::thread th2(g);
     m.lock();
     while (!g_ready)
@@ -113,12 +145,24 @@ void testCVAnyMutex()
     std::thread th1(f);
     th1.join();
     th2.join();
+    free(raw);
+  std::cout << "\n*** OK" << std::endl;
 }
-#else
+
+#else // ORIG_CVANY_RACE_TEST
 
 void testCVAnyMutex() 
 {
-  std::condition_variable_any2* cv = new std::condition_variable_any2;
+  // test the basic jthread API
+  std::cout << "*** start testCVAnyMUtex()" << std::endl;
+
+  // AW 9.11.18: 
+  // Writing over the deleted memory is undefined behaviour. In particular,
+  // it can destroy the heap data structure, and cause other problems.
+  // If you replace new/delete with malloc and free, then it's OK
+  //std::condition_variable_any2* cv = new std::condition_variable_any2;
+  void* raw=malloc(sizeof(std::condition_variable_any2));
+  std::condition_variable_any2* cv = new(raw) std::condition_variable_any2;
   std::mutex m;
   bool f_ready = false;
   bool g_ready = false;
@@ -146,8 +190,11 @@ void testCVAnyMutex()
                  });
   t1.join();
   t2.join();
+  free(raw);
+  std::cout << "\n*** OK" << std::endl;
 }
-#endif
+#endif // ORIG_CVANY_RACE_TEST
+
 
 //------------------------------------------------------
 
